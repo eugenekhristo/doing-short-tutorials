e839cd3c33842663c21b3ac5f7f0d2da6615df7e
Eugene Khristo <marteau9292@gmail.com>
2019-03-01 20:36:59 +0300

    Super way of exporting and requiring files

--- the-net-ninja-node-for-begginers/app.js
+++ the-net-ninja-node-for-begginers/app.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
+const {add, minus} = require('./utils');
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
+
+console.log(add(2, 3));
+console.log(minus(7, 3));
