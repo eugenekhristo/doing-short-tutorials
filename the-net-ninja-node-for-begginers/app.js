78d96fdb16f6264fa61c154921f09bd283d82de3
Eugene Khristo <marteau9292@gmail.com>
2019-03-07 20:48:55 +0300

    Streams and pipes into Res obj

--- the-net-ninja-node-for-begginers/app.js
+++ the-net-ninja-node-for-begginers/app.js
@@ -2,9 +2,10 @@ const fs = require('fs');
 const http = require('http');
 
 const readStream = fs.createReadStream(__dirname + '/readMe.txt');
-const writeStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
-readStream.on('data', chunk => {
-  console.log('Chunk recieved: ');
-  writeStream.write(chunk);
+const myServer = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  readStream.pipe(res);
 });
+
+myServer.listen(3000, '192.168.0.100');
