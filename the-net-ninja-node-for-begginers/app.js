22ef805e9557c546476af3e317da3653c7e8cc97
Eugene Khristo <marteau9292@gmail.com>
2019-03-07 21:21:26 +0300

    Served JSON file

--- the-net-ninja-node-for-begginers/app.js
+++ the-net-ninja-node-for-begginers/app.js
@@ -2,9 +2,13 @@ const fs = require('fs');
 const http = require('http');
 
 const myServer = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  const readStream = fs.createReadStream(__dirname + '/index.html');
-  readStream.pipe(res);
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  const person = {
+    name: 'Eugene',
+    age: 26
+  };
+
+  res.end(JSON.stringify(person)); 
 });
 
 myServer.listen(3000, '192.168.0.100');
