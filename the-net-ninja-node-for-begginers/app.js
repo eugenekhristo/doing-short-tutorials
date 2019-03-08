3a4470a8488c5d4b9136bb068801e81a3bd66f12
Eugene Khristo <marteau9292@gmail.com>
2019-03-08 14:48:50 +0300

    EJS is added and built dynamic page

--- the-net-ninja-node-for-begginers/app.js
+++ the-net-ninja-node-for-begginers/app.js
@@ -1,21 +1,33 @@
-const fs = require('fs');
-const http = require('http');
-const jsonData = require('./someData.json');
+require('colors');
+const express = require('express');
 
-const myServer = http.createServer((req, res) => {
-  if (req.url === '/' || req.url === '/home') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  } else if (req.url === '/contacts') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream(__dirname + '/contacts.html').pipe(res);
-  } else if (req.url === '/api/data') {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(jsonData));
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.createReadStream(__dirname + '/404.html').pipe(res);
-  }
+const app = express();
+// say to express that we are going to use EJS as template engine
+// so it will go look at views folder automatically
+app.set('view engine', 'ejs');
+
+// send static HTML
+app.get('/', (req, res) => {
+  res.status(200).sendFile(`${__dirname}/index.html`);
 });
 
-myServer.listen(3000, '192.168.0.100');
+app.get('/contacts', (req, res) => {
+  res.status(200).sendFile(`${__dirname}/contacts.html`);
+});
+
+// send DYNAMIC HTML (ejs)
+app.get('/profile/:name', (req, res) => {
+  const { name } = req.params;
+  const person = {
+    name,
+    age: 26,
+    city: 'Minsk',
+    hobbies: ['eating 🍕', 'fighting  🐱‍👓', 'fishing  🐟']
+  };
+  res.render('profile', { person });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>
+  console.log(`NodeJS server is running on the port ${PORT}...`.bgCyan)
+);
