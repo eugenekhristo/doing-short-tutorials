e98a15b35c258c7eef6f7549b4458f477ca9c641
Eugene Khristo <marteau9292@gmail.com>
2019-03-08 12:11:53 +0300

    Basic routing

--- the-net-ninja-node-for-begginers/app.js
+++ the-net-ninja-node-for-begginers/app.js
@@ -1,14 +1,21 @@
 const fs = require('fs');
 const http = require('http');
+const jsonData = require('./someData.json');
 
 const myServer = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  const person = {
-    name: 'Eugene',
-    age: 26
-  };
-
-  res.end(JSON.stringify(person)); 
+  if (req.url === '/' || req.url === '/home') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if (req.url === '/contacts') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream(__dirname + '/contacts.html').pipe(res);
+  } else if (req.url === '/api/data') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(jsonData));
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+  fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
 });
 
 myServer.listen(3000, '192.168.0.100');
