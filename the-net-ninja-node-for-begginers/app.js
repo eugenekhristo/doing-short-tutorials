e91e53f2a04dd62935084bfebfd551553e8f039c
Eugene Khristo <marteau9292@gmail.com>
2019-03-09 11:56:32 +0300

    Query strings and body parser

--- the-net-ninja-node-for-begginers/app.js
+++ the-net-ninja-node-for-begginers/app.js
@@ -1,18 +1,28 @@
 require('colors');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 // say to express that we are going to use EJS as template engine
 // so it will go look at views folder automatically
 app.set('view engine', 'ejs');
+// if requrest starts with /assets - it means express will send static files
+// from assets folder (fiolder can be named whatever)
+app.use('/assets', express.static('assets'));
+
 
 // send static HTML
 app.get('/', (req, res) => {
-  res.status(200).sendFile(`${__dirname}/index.html`);
+  res.render('index');
 });
 
 app.get('/contacts', (req, res) => {
-  res.status(200).sendFile(`${__dirname}/contacts.html`);
+  res.render('contacts');
+});
+
+app.post('/quotes', urlencodedParser, (req, res) => {
+  const {title, content} = req.body;
+  res.render('quote', {title, content});
 });
 
 // send DYNAMIC HTML (ejs)
