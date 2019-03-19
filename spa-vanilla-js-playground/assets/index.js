653136ba264ac03ed75eef939596385b097ba222
Eugene <marteau9292@gmail.com>
2019-03-19 08:13:36 +0300

    API endpoints draft

--- spa-vanilla-js-playground/assets/index.js
+++ spa-vanilla-js-playground/assets/index.js
@@ -15,18 +15,14 @@ let contactPage = `
 const routes = {
   '/': homePage,
   '/about': aboutPage,
-  '/contact': contactPage,
+  '/contact': contactPage
 };
 
 const contentDiv = document.getElementById('content');
 contentDiv.innerHTML = routes[window.location.pathname];
 
-let onNavItemClick = (pathName) => {
-  window.history.pushState(
-    null, 
-    '',
-    window.location.origin + pathName
-  );
+let onNavItemClick = pathName => {
+  window.history.pushState(null, '', window.location.origin + pathName);
 
   if (pathName.includes('?q')) {
     const queryParams = new URLSearchParams(window.location.search);
@@ -34,8 +30,32 @@ let onNavItemClick = (pathName) => {
   }
 
   contentDiv.innerHTML = routes[pathName.split('?')[0]];
-}
+};
 
 window.onpopstate = () => {
   contentDiv.innerHTML = routes[window.location.pathname];
-}
+};
+
+// SERVICES
+
+// // get several
+// // 'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=C1HjBbRbN4I6lKblBGpY1bxcpdopyH6T&offset=5&limit=10'
+
+// // single
+// // http://api.giphy.com/v1/gifs/feqkVgjJpYtjy?api_key=C1HjBbRbN4I6lKblBGpY1bxcpdopyH6T
+
+// let images = '';
+
+// fetch(
+//   'http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=C1HjBbRbN4I6lKblBGpY1bxcpdopyH6T&offset=5&limit=10'
+// )
+//   .then(res => res.json())
+//   .then(({ data }) => {
+//     data.forEach(item => {
+//       const { mp4 } = item.images.fixed_width;
+
+//       images += `<video src="${mp4}" autoplay loop></video>`;
+//     });
+
+//     document.body.insertAdjacentHTML('afterbegin', images);
+//   });
