97ccf57513a5de19c681e7b50b6601842a679965
Eugene Khristo <marteau9292@gmail.com>
2019-03-20 15:42:11 +0300

    Refactored a bit

--- spa-vanilla-js-playground/assets/js/state.js
+++ spa-vanilla-js-playground/assets/js/state.js
@@ -1,10 +1,17 @@
+import { getQueryStringValueOfCurrentPage } from "./utils.js";
+
 // state of input field when search page is loaded
-let inputValue = '';
+let inputValue = "";
 // query string for giphy in format: x+xxx+xx
-let queryString = '';
+let queryString = getQueryStringValueOfCurrentPage() || "";
 // current offset of images for query URL
 let thumbnailsOffset = 0;
 // HTML string for gallery on search page
 let galleryThumbnailsHTML = "";
 
-export default {inputValue, queryString, thumbnailsOffset, galleryThumbnailsHTML}
+export default {
+  inputValue,
+  queryString,
+  thumbnailsOffset,
+  galleryThumbnailsHTML
+};
