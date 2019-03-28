7a7b622da4ece17bf1902902e73833fda652f0ea
Eugene Khristo <marteau9292@gmail.com>
2019-03-28 14:32:20 +0300

    DONE!

--- spa-vanilla-js-playground/assets/js/core/state.js
+++ spa-vanilla-js-playground/assets/js/core/state.js
@@ -1,17 +1,12 @@
 import { getQueryStringValueOfCurrentPage } from './utils.js';
 
-let thumbnailsOffsetInQueryString = 0;
-let queryStringValue = getQueryStringValueOfCurrentPage() || '';
-let isAppLoadedForTheFirstTime = true;
-let galleryThumbnailsHTML = '';
-let previousRoutePathnameAndSearch =  {
-  pathname: window.location.pathname,
-  search: window.location.search
-};
-
 export default {
-  queryStringValue,
-  thumbnailsOffsetInQueryString,
-  isAppLoadedForTheFirstTime,
-  galleryThumbnailsHTML,
-  previousRoutePathnameAndSearch};
+  queryStringValue: getQueryStringValueOfCurrentPage() || '',
+  thumbnailsOffsetInQueryString: 0,
+  isAppLoadedForTheFirstTime: true,
+  galleryThumbnailsHTML: '',
+  previousRoutePathnameAndSearch: {
+    pathname: window.location.pathname,
+    search: window.location.search
+  }
+};
